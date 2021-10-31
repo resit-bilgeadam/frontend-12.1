f02c52b5a09f17a3aa480a0c204129449c3400da
resit-bilgeadam <resit.bilgedam@gmail.com>
2021-10-31 13:48:24 +0300

    add button id

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 console.log('hello');
 
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('#btn');
 btn.addEventListener('click', (e) => {
     alert('Button clicked!!');
 })
